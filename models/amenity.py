4a57bb82a4145b698809c64eb2f5f32c8cc66a07
JohnathanHarbertHolder <holderjohnathanh@gmail.com>
2022-11-27 18:38:29 -0800

    Team Project

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the amenity class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from os import getenv
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+        __tablename__: table Amenities
+        name: input name
+    """
+    __tablename__ = "amenities"
+    name = Column("name", String(128), nullable=False)
